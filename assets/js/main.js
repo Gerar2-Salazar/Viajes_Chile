9644822ca47757e0e641fd1ab8228f22b7f75d28
Gerardo Salazar <157648035+Gerar2-Salazar@users.noreply.github.com>
2025-05-08 18:14:20 +0000

    av.7 Viajes

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,21 +11,43 @@ $(document).ready(function () {
     $('["data-bs-toggle="tooltip"]').tooltip();
   });
 
-
-$('#correo').click(function () {
-    alert("Correo enviado correctamente")
-
+  $('#correo').click(function () {
+    alert("Correo enviado correctamente") 
 })
-
 
 //Cambiar color//
 $(document).ready(function() {
-  $('#Destacados').on('dblclick', function() {
-    $(this).css('color', 'red'); // Cambia el color a rojo
+  $('#destacados').on('dblclick', function() {
+    $(this).css('color', 'cyan'); // Cambia el color a rojo
   });
 
-  $('#contacto').on('dblclick', function() {
-    $(this).css('color', 'red'); // Cambia el color a rojo
+  $('#contactos').on('dblclick', function() {
+    $(this).css('color', 'cyan'); // Cambia el color a rojo
   });
 });
 
+//Ocultar información//
+
+$(document).ready(function () {
+  $('#card-title').on('click', function () {
+    $(this).siblings('.card-text').toggle();
+  });
+});
+
+$(document).ready(function () {
+  $('#card-title1').on('click', function () {
+    $(this).siblings('.card-text').toggle();
+  });
+});
+
+$(document).ready(function () {
+  $('#card-title2').on('click', function () {
+    $(this).siblings('.card-text').toggle();
+  });
+});
+
+$(document).ready(function () {
+  $('#card-title3').on('click', function () {
+    $(this).siblings('.card-text').toggle();
+  });
+});
